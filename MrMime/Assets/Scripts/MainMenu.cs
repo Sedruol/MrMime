d4308b17ba65fb79d8b6730c56003617bff1b626
Sedruol <44387684+Sedruol@users.noreply.github.com>
2020-11-09 18:28:34 -0500

    first version, menu's interaction

---
+++ MrMime/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button btnAddMovement;
+    [SerializeField] private Button btnSelectMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnAddMovement.onClick.AddListener(() => GoAddMovement());
+        btnSelectMovement.onClick.AddListener(() => GoSelectMovement());
+    }
+    public void GoAddMovement()
+    {
+        SceneManager.LoadScene("Add Movement");
+    }
+    public void GoSelectMovement()
+    {
+        SceneManager.LoadScene("Select Movement");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MrMime/Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button btnGoHome;
+    [SerializeField] private Button btnExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnGoHome.onClick.AddListener(() => GoHome());
+        btnExit.onClick.AddListener(() => Exit());
+    }
+
+    private void GoHome()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    private void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
