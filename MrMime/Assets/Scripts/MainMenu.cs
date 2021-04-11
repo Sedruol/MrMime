0f4cf57bcd0331c1242cd967a1776334d58fe9ed
DiegoJohnsonUPC <44352982+DiegoJohnsonUPC@users.noreply.github.com>
2021-04-11 11:42:06 -0500

    robot arm simulator

--- MrMime/Assets/Scripts/MainMenu.cs
+++ MrMime/Assets/Scripts/MainMenu.cs
@@ -24,13 +24,17 @@ public class MainMenu : MonoBehaviour
     }*/
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(Screen.width * 0.41f, Screen.height * 0.5f, 150, 50), "Add Movement"))
+        if (GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f, Screen.width*0.1f, Screen.height * 0.05f), "Add Movement"))
         {
             SceneManager.LoadScene("Add Movement 1");
         }
-        if (GUI.Button(new Rect(Screen.width * 0.41f, Screen.height * 0.75f, 150, 50), "Select Movement"))
+        if (GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.6f, Screen.width * 0.1f, Screen.height * 0.05f), "Select Movement"))
         {
             SceneManager.LoadScene("Select Movement");
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.7f, Screen.width * 0.1f, Screen.height * 0.05f), "Try simulator"))
+        {
+            SceneManager.LoadScene("RobotArm");
         }
     }
     // Update is called once per frame
